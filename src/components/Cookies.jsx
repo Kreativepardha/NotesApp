a210e08ee19aab1b726351e8cb14665e8be47f4a
Kreativepardha <116539060+Kreativepardha@users.noreply.github.com>
2024-02-09 11:57:50 +0530

    Update Cookies.jsx

--- src/components/Cookies.jsx
+++ src/components/Cookies.jsx
@@ -13,7 +13,7 @@ export const Cookie = ()=>{
                     location='bottom'
                     buttonText="Accept All Cookies"
                     cookieName='myCookieConsent'
-                    style={{background:'linear-gradient(140deg,wheat,bisque)', height:"150px", color:"black",boxShadow:"8px 8px black" ,width:"300px",left:"60px",top:"650px",borderRadius:"8px"}}
+                    style={{background:'linear-gradient(140deg,cyan,aquamarine)', height:"150px", color:"black",boxShadow:"8px 8px black" ,width:"300px",left:"60px",top:"250px",borderRadius:"8px"}}
                     buttonStyle={{ color: "black", fontSize: "16px",boxShadow:"2px 2px black" ,borderRadius:"8px" }}
                     expires={150}
                     >
